159de908bee9340d297db0a27b74719adce193b6
Carlos Hernandez <cxh@stanford.edu>
2015-04-11 04:51:49 -0700

    add backup locator

--- scripts/iplookup.py
+++ scripts/iplookup.py
@@ -3,24 +3,37 @@ import json
 import numpy as np
 import pandas as pd
 import geoip2.database
+from geopy.geocoders import Nominatim
 
 
 if len(sys.argv) != 3:
     sys.exit('Please specify a GeoLite DB and an ip table.')
 
 reader = geoip2.database.Reader(sys.argv[1])
+geolocator = Nominatim()
+places = ['city', 'town', 'county', 'state']
 
 
-def get_name(entry, lang):
+def get_city_from_coord(coord):
+    try:
+        address = geolocator.reverse(coord, timeout=10).raw['address']
+        for place in places:
+            if place in address.keys() and not address[place][1:].isdigit():
+                return address[place]
+    except:
+        print 'A timeout occurred.'
+    return 'unknown'
+
+
+def get_city(entry, lang):
     if hasattr(entry, 'names') and lang in entry.names:
         return entry.names[lang]
-
     return 'unknown'
 
 
 def get_location(addr):
     response = reader.city(addr)
-    city = get_name(response.city, 'en')
+    city = get_city(response.city, 'en')
     lat = response.location.latitude
     lng = response.location.longitude
     return (city, lat, lng)
@@ -36,6 +49,8 @@ ucounts = series.value_counts()
 info = []
 for location, count in zip(ucounts.keys(), ucounts.get_values()):
     if location:
+        if location[0] == 'unknown':
+            location[0] == get_city_from_coord(location[1:])
         info.append({'city_name': location[0],
                      'lat': location[1],
                      'long': location[-1],
